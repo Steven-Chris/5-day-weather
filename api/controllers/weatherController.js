6d59bb962401528ba94dd75eb9787c1a9b6558ce
steven.chris <steven.chris_entvickler.com#EXT#@DoctorsGroup237.onmicrosoft.com>
2022-03-30 20:35:36 +0530

    Geolocation api done

--- api/controllers/weatherController.js
+++ api/controllers/weatherController.js
@@ -1,13 +1,18 @@
 "use strict";
 const constantConfig = require("../../config/constantConfig");
-const { handleError } = require("../lib/errorHandling");
-const Product = require("../../models/productModel");
+const axios = require("axios");
 
 module.exports.getWeather = async (req, res) => {
+  const api_key = process.env.API_KEY;
   try {
+    const data = await axios.get(
+      `https://api.openweathermap.org/data/2.5/forecast?lat=18.6298&lon=73.7997&appid=${api_key}&units=metric`
+    );
+    console.log(data?.data.list.length);
     res.status(constantConfig.SUCCESS_CODE).json({
       status: constantConfig.SUCCESS,
       message: `Here is your weather`,
+      data: data?.data,
     });
   } catch (err) {
     console.log(`err`, err);
